152e5c107aa6e9c23e792af9ee256f8d0d3dd0e5
bryan.bedoya <bryan.bedoya@pragma.com.co>
2017-11-12 12:36:43 -0500

    angular's project initial configuration views, controllers, services, interceptor contents: css and js scripts: local storage angular, loading bar

---
+++ EscuelaAngular/app/services/teachersService.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+app.factory('teachersService', ['$http', function ($http) {
+
+    var serviceBase = 'http://localhost:57778/';
+    var teachersServiceFactory = {};
+
+    var _getTeachers = function () {
+
+        return $http.get(serviceBase + 'api/teachers').then(function (results) {
+            return results;
+        });
+    };
+
+    teachersServiceFactory.getTeachers = _getTeachers;
+
+    return teachersServiceFactory;
+
+}]);
